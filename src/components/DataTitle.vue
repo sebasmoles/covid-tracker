b91265048f1962955f11a9e98a2c3398c90c2d97
Sebastian <roshfeltsemo@gmail.com>
2021-10-22 09:23:41 -0500

    Data boxes and Data title components  added

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-center">
+        <h2 class="text-3xl font-bold">{{title}}</h2>
+        <h3 class="text-2xl mt-4 mb-10">{{timestamp}}</h3>
+    </div>
+</template>
+
+<script>
+    import moment from 'moment';
+
+    export default {
+        name: 'DataTitle',
+        props: ['title', 'dataDate'],
+        computed: {
+            timestamp: function () {
+               return moment(this.dataDate).format('MMMM Do YYYY, h:mm:ss a');
+            }
+        }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,48 @@
 <template>
-  <div class="home">
-    Hello World
-  </div>
+  <main v-if="!loading ">
+    <DataTitle :title="title" :dataDate="dataDate"/>
+
+    <DataBoxes :stats="stats" />
+  </main>
+  <main v-else class="flex justify-center">
+    <img :src="loadingImage" />
+  </main>
 </template>
 
 <script>
+  import DataTitle from '../components/DataTitle.vue';
+  import DataBoxes from '../components/DataBoxes.vue'
 
-export default {
-  name: 'Home',
-  components: {
+  export default {
+    name: 'Home',
+    components: {
+      DataTitle,
+      DataBoxes
+    },
+    data() {
+      return {
+        loading: true,
+        title: 'Global',
+        dataDate: '',
+        stats: {},
+        countries: [],
+        loadingImage: require('../assets/loading.gif')
+      }
+    },
+    methods: {
+      async fetchCovidData() {
+        const res = await fetch('https://api.covid19api.com/summary');
+        const data = await res.json();
+        return data;
+      }
+    },
+    async created() {
+      const data = await this.fetchCovidData();
 
+      this.dataDate = data.Date;
+      this.stats = data.Global; 
+      this.countries = data.Countries;
+      this.loading = false;
+    }
   }
-}
 </script>
